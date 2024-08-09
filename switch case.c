cc674b016a0a66cfb32c5f9f1926bc7d6a4ae509
Ayshee782 <152168245+Ayshee782@users.noreply.github.com>
2024-08-09 13:03:32 +0600

    Add files via upload

---
+++ switch case.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+    int day ;
+
+    printf("%d enter day= ");
+    scanf("%d",& day);
+    switch(day){
+        case 1: {
+        printf("friday\n");
+        break;
+        }
+        case 2:
+            {
+                printf("saturday\n");
+                break;
+            }
+        case 3:
+            {
+                printf("sunday\n");
+                break;
+
+            }
+        case 4 :
+            {
+                printf("monday\n");
+                break;
+            }
+        case 5 :
+            {
+                printf("tusday\n");
+                break;
+            }
+        case 6:
+            {
+                printf("wednesday\n");
+                break;
+            }
+        case 7:
+            {
+                printf("thursday\n");
+                break;
+            }
+        default:
+            {
+                printf("not exist!");
+            }
+
+    }
+
+
+}
